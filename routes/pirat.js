8e0dd8ed7bd59c957b220d75744d15f51a39e6a3
Rampo8 <ablyalimoval@gmail.com>
2025-12-04 22:20:17 +0300

    'pirate_8_3'

--- routes/pirat.js
+++ routes/pirat.js
@@ -1,14 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var pirat = require('../models/pirats').pirat;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Новый маршрутизатор, для маршрутов, начинающихся с scoobys');
+  res.send('Новый маршрутизатор, для маршрутов, начинающихся с pirats');
 });
 /* Страница Корабль */
-router.get("/:nick", function(req, res, next) {
-    res.send(req.params.nick);
+router.get("/:nick", async function(req, res, next) {
+   var pirats = await pirat.find({nick: req.params.nick});
+   console.log(pirats)
+   if(!pirats.length) return next(new Error("Нет такого пирата в One piece"))
+       var pirat = pirats[0];
+       res.render('pirat', {
+           title: pirat.title,
+           picture: pirat.avatar,
+           desc: pirat.desc
+       })
 });
+
 
 
 module.exports = router;
