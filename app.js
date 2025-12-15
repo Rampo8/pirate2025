e1750883cb1b0d4bfbd1f4fe6f7eafea5f60ead8
Rampo8 <ablyalimoval@gmail.com>
2025-12-16 01:12:47 +0300

    'pirate_9_4'

--- app.js
+++ app.js
@@ -31,6 +31,10 @@ app.use(session({
  saveUninitialized: true,
  store: MongoStore.create({mongoUrl: 'mongodb://localhost/pirats2025'})
 }))
+app.use(function(req,res,next){
+ req.session.counter = req.session.counter + 1 || 1
+ next()
+ })
 
 
 app.use('/', indexRouter);
