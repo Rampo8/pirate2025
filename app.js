5cb5fe22485ab4d241eb44dde24d826989526089
Rampo8 <ablyalimoval@gmail.com>
2025-12-18 08:34:16 +0300

    'pirate_9_5'

--- app.js
+++ app.js
@@ -9,14 +9,11 @@ var session = require("express-session")
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var indexpirate = require('./routes/pirats');
-
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.engine('ejs',require('ejs-locals'));
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -30,13 +27,12 @@ app.use(session({
  resave: true,
  saveUninitialized: true,
  store: MongoStore.create({mongoUrl: 'mongodb://localhost/pirats2025'})
-}))
+}));
 app.use(function(req,res,next){
  req.session.counter = req.session.counter + 1 || 1
  next()
  })
-
-
+app.use(require("./middlewares/createMenu.js"))
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/pirat', indexpirate);
@@ -44,18 +40,14 @@ app.use('/pirat', indexpirate);
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error',{title: 'One piece'});
 
 });
-
-
 module.exports = app;
