48dd410f540762af33f69abc84b0c3186095abcf
Rampo8 <ablyalimoval@gmail.com>
2025-12-18 09:26:42 +0300

    'pirate_10_2'

--- app.js
+++ app.js
@@ -19,20 +19,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(require("./middlewares/createMenu.js"))
+
 var MongoStore = require('connect-mongo');
+
 app.use(session({
- secret: "Nakama",
- cookie:{maxAge:60*1000},
- proxy: true,
- resave: true,
- saveUninitialized: true,
- store: MongoStore.create({mongoUrl: 'mongodb://localhost/pirats2025'})
+  secret: "Nakama",
+  cookie: { maxAge: 60 * 1000 },
+  proxy: true,
+  resave: true,
+  saveUninitialized: true,
+  store: MongoStore.create({
+    client: mongoose.connection.getClient(),
+    dbName: 'pirats2025'
+  })
 }));
 app.use(function(req,res,next){
  req.session.counter = req.session.counter + 1 || 1
  next()
  })
-app.use(require("./middlewares/createMenu.js"))
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/pirat', indexpirate);
