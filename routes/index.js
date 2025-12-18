f276df79eb081fd05c1094a9fbd81b514e1bc6be
Rampo8 <ablyalimoval@gmail.com>
2025-12-18 09:30:07 +0300

    'pirate_10_3'

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var router = express.Router();
-
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'pirate2025' });
+router.post('/logreg', function(req, res, next) {
+  var username = req.body.username
+  var password = req.body.password
+  console.log(username);
+  console.log(password);
 });
+
 router.get('/logreg', function(req, res, next) {
  res.render('logreg',{title: 'Вход'});
  });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'pirate2025' });
+});
+
 
 module.exports = router;
