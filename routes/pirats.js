06657bafbf45eb9f42c5e8f4fd015610bebb2ed1
Rampo8 <ablyalimoval@gmail.com>
2025-12-18 10:03:17 +0300

    pirate_10_8

--- routes/pirats.js
+++ routes/pirats.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
   res.send('Новый маршрутизатор, для маршрутов, начинающихся с pirats');
 });
 /* Страница Корабль */
-router.get("/:nick", async function(req, res, next) {
+router.get("/:nick", checkAuth, async function(req, res, next) {
    var pirats = await pirat.find({nick: req.params.nick});
    console.log(pirats)
    if(!pirats.length) return next(new Error("Нет такого пирата в One piece"))
