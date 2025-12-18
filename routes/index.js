62d467abe7c2c39236238b0b67a241a514c1b21f
Rampo8 <ablyalimoval@gmail.com>
2025-12-18 08:38:32 +0300

    pirate_1-_1

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'pirate2025' });
 });
+router.get('/logreg', function(req, res, next) {
+ res.render('logreg',{title: 'Вход'});
+ });
+
 module.exports = router;
