00c4f60e7c6b0d68c33c0d039d49fa3e68541103
Rampo8 <ablyalimoval@gmail.com>
2025-12-20 19:23:28 +0300

    pirate_10_8

--- middlewares/createUser.js
+++ middlewares/createUser.js
@@ -1,13 +1,10 @@
 var User = require("../models/user").User;
 
-
-module.exports = async function(req,res,next){
-   res.locals.user = null
-
-
-  var users =  await User.findById(req.session.user_id);
-  if (users.length != 0) {
-   res.locals.user = users[0];
+module.exports = async function (req, res, next) {
+  res.locals.user = null;
+  var user = await User.findById(req.session.user_id);
+  if (user) {
+    res.locals.user = user;
   }
   next();
-}
+};
