e1081862b73696febeb6be36df8a3f85b381b700
Rampo8 <ablyalimoval@gmail.com>
2025-12-20 19:31:14 +0300

    pirate_9_3

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,8 @@
-var express = require('express');
-router.get('/', function(req, res, next) {
- req.session.greeting = "Hi!!!";
- res.render('index', { title: 'Express' });
-});
+var express = require("express");
+var router = express.Router();
 
-
-/* Страница Luffy*/
-router.get('/pirat/Luffy', function(req, res, next) {
-    res.render('pirat', {
-        title: "Монки Ди Луффи ",
-        picture: "images/luffy.png",
-        desc: "Луфи — главный герой аниме «Ван Пис». Весёлый и целеустремлённый пират, мечтающий стать Королём Пиратов. Обладает резиновым телом после поедания Дьявольского плода Гому-Гому. Его безудержная энергия и верность друзьям вдохновляют."
-    });
-});
-
-/* Страница Roger */
-router.get('/pirat/Roger', function(req, res, next) {
-     res.render('pirat', {
-        title: "Голд Ди Роджер",
-        picture: "images/roger.png",
-        desc: "Гол Д. Роджер — легендарный Король Пиратов, достигший последнего острова Лаф Тейл. Его казнь положила начало «Великой эпохе пиратства». Сильный, веселый и вдохновляющий лидер."
-    });
-});
-
-/* Страница Ace */
-router.get('/pirat/Ace', function(req, res, next) {
-    res.render('pirat', {
-        title: "Поргас Ди Эйс",
-        picture: "images/ace.png",
-        desc: "Эйс — командир 2-го дивизиона пиратов Белоуса. Сильный и благородный, носитель могущественного Плода Логгии Огня. Его трагическая гибель ради младшего брата стала поворотным моментом для всего мира."
-    });
+router.get("/", function (req, res, next) {
+  req.session.greeting = "Hi!!!";
+  res.render("index", { title: "Express" });
 });
 module.exports = router;
